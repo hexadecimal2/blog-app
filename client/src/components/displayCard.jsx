c5e0b6ba86768ae06ce041af5e9276265ab0f0b7
hexadecimal2 <kidlemon67@gmail.com>
2024-09-19 12:43:30 +0200

    added .env

--- client/src/components/displayCard.jsx
+++ client/src/components/displayCard.jsx
@@ -11,6 +11,8 @@ const DisplayCard = (props) => {
         <h1>{props.data.Title}</h1>
         <h3>{props.data.Date}</h3>
         <h2>{props.data.Description}</h2>
+        <h4> by {props.data.Author} </h4>
+        
         </div>
     );
 
